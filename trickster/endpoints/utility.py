66f497da9384df537c33b880e090087c353b4cb6
Michal Pomykacz <michal@pomyka.cz>
2021-04-24 09:49:29 +0200

    add react frontend

--- trickster/endpoints/utility.py
+++ trickster/endpoints/utility.py
@@ -2,13 +2,29 @@
 
 from __future__ import annotations
 
-from flask import Blueprint, Response, jsonify
+import os
+from pathlib import Path
+
+from flask import Blueprint, Response, jsonify, send_from_directory
 
 
 endpoints = Blueprint('utility_api', __name__)
+
+STATIC_ROOT = os.path.join(Path().resolve(strict=True), 'frontend/build')
 
 
 @endpoints.route('/health', methods=['GET'])
 def health() -> Response:
     """Returns internal app status."""
     return jsonify({'status': 'ok'})
+
+
+@endpoints.route('/static', methods=['GET'])
+def index() -> Response:
+    """Returns internal app status."""
+    return send_from_directory(STATIC_ROOT, 'index.html')
+
+
+@endpoints.route('/static/<path:path>')
+def serve_static_file(path):
+    return send_from_directory(STATIC_ROOT, path)
