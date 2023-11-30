6c8acd7a1a76bd53938f59e30e5f59f0817d6253
Jakub Tesarek <jakub@tesarek.me>
2023-11-30 15:15:15 +0100

    feat: add support for custom error messages

--- tests/config.py
+++ tests/config.py
@@ -72,6 +72,18 @@ class TestJsonConfigSettingsSource:
         with pytest.raises(ConfigError):
             JsonConfigSettingsSource(Config)
 
+    def test_load_default_config(self, tmpdir, mocker):
+        config_file = tmpdir.join('config.json')
+        config_file.write(json.dumps(config_data))
+        mocker.patch(
+            'trickster.config.JsonConfigSettingsSource.default_config_file_path',
+            new_callable=mocker.PropertyMock,
+            return_value=str(config_file)
+        )
+
+        config_source = JsonConfigSettingsSource(Config)
+        assert config_source() == config_data
+
     def test_get_field_value(self, tmpdir, mocker):
         config_file = tmpdir.join('config.json')
         config_file.write(json.dumps(config_data))
